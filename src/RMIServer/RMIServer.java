8fa871671e51055f31e64325ac384dba1eaa6c65
nasif1731 <nasif1731@gmail.com>
2025-02-18 00:04:30 +0500

    Kitaab_Script Server

--- src/RMIServer/RMIServer.java
+++ src/RMIServer/RMIServer.java
@@ -1,8 +1,13 @@
 package RMIServer;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Properties;
+
+import javax.swing.JOptionPane;
 
 import bll.BLFacade;
 import bll.FileBO;
@@ -19,69 +24,51 @@ import dal.AbstractDALFactory;
 import dal.DALFacade;
 import dal.IDALFacade;
 import dal.IDALFactory;
-import dal.IFileDAO;
-import dal.IFileImportDAO;
-import dal.ILemmatizationDAO;
-import dal.IPOSTaggingDAO;
-import dal.IPaginationDAO;
-import dal.ISearchResultDAO;
-import dal.ITransliterationDAO;
 import interfaces.IBLFacade;
-import interfaces.IFileBO;
-import interfaces.IFileImportBO;
-import interfaces.IFilePaginationBO;
-import interfaces.IKLAnalysisBO;
-import interfaces.ILemmatizationBO;
-import interfaces.IPMIAnalysisBO;
-import interfaces.IPOSTaggingBO;
-import interfaces.ISearchResultBO;
-import interfaces.ITFIDFAnalysisBO;
-import interfaces.ITransliterationBO;
-
 public class RMIServer {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		IDALFactory dalFactory = AbstractDALFactory.getInstance();
-        
-        IFileDAO fileDAO = dalFactory.getFileDAO();
-        IFileImportDAO fileImportDAO = dalFactory.getFileImportDAO();
-        IPaginationDAO paginationDAO = dalFactory.getPaginationDAO();
-        ISearchResultDAO searchResultDAO=dalFactory.getSearchResultDAO();
+    public static void main(String[] args) throws RemoteException {
+        try {
+            // Load configurations
+            Properties config = new Properties();
+            config.load(new FileInputStream("config.properties"));
 
-        ITransliterationDAO transliterationDAO=dalFactory.getTransliterationDAO();  
-        ILemmatizationDAO lemmatizationDAO=dalFactory.getLemmatizationDAO();
-        IPOSTaggingDAO postaggingDAO=dalFactory.getPOSTaggingDAO();
-        
-        
-        IDALFacade dalFacade = new DALFacade(fileDAO, fileImportDAO, paginationDAO,searchResultDAO,transliterationDAO,lemmatizationDAO,postaggingDAO);
+            String host = config.getProperty("server.host");
+            int port = Integer.parseInt(config.getProperty("server.port"));
 
-        
-        IFileBO fileBO = new FileBO(dalFacade);
-        IFileImportBO fileImportBO = new FileImportBO(dalFacade);
-        IFilePaginationBO filePaginationBO = new FilePaginationBO(dalFacade);
-        ISearchResultBO searchResultBO=new SearchResultBO(dalFacade);
+            // Initialize DAL and BLL components
+            IDALFactory dalFactory = AbstractDALFactory.getInstance();
+            IDALFacade dalFacade = new DALFacade(
+                dalFactory.getFileDAO(), 
+                dalFactory.getFileImportDAO(), 
+                dalFactory.getPaginationDAO(), 
+                dalFactory.getSearchResultDAO(), 
+                dalFactory.getTransliterationDAO(), 
+                dalFactory.getLemmatizationDAO(), 
+                dalFactory.getPOSTaggingDAO()
+            );
 
-        ITransliterationBO transliterationBO=new TransliterationBO(dalFacade);
-        ILemmatizationBO lemmatizationBO=new LemmatizationBO(dalFacade);
-        IPOSTaggingBO posTaggingBO =new POSTaggingBO (dalFacade);
-        ITFIDFAnalysisBO tfidfAnalysisBO=new TFIDFAnalysisBO(dalFacade);
-        IPMIAnalysisBO pmiAnalysisBO=new PMIAnalysisBO(dalFacade);
-        IKLAnalysisBO klAnalysisBO=new KLAnalysisBO(dalFacade);
-        IBLFacade blFacade;
-		try {
-			blFacade = new BLFacade(fileBO,fileImportBO,filePaginationBO,searchResultBO,transliterationBO,lemmatizationBO,posTaggingBO,tfidfAnalysisBO,pmiAnalysisBO,klAnalysisBO);
-			Registry registry;
-			registry = LocateRegistry.createRegistry(1099);
-			registry.rebind("BLFacade", blFacade);
-			System.out.println("Server is running...");
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        
-	
-		
-	}
+            IBLFacade blFacade = new BLFacade(
+                new FileBO(dalFacade), 
+                new FileImportBO(dalFacade), 
+                new FilePaginationBO(dalFacade), 
+                new SearchResultBO(dalFacade), 
+                new TransliterationBO(dalFacade), 
+                new LemmatizationBO(dalFacade), 
+                new POSTaggingBO(dalFacade), 
+                new TFIDFAnalysisBO(dalFacade), 
+                new PMIAnalysisBO(dalFacade), 
+                new KLAnalysisBO(dalFacade)
+            );
 
+            // Start the RMI server
+            Registry registry = LocateRegistry.createRegistry(port);
+            registry.rebind("BLFacade", blFacade);
+            JOptionPane.showMessageDialog(null, "Server is running");
+
+        } catch (IOException e) {
+        	JOptionPane.showMessageDialog(null, "Server exception");
+            e.printStackTrace();
+        }
+    }
 }
